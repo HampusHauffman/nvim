e4d7c784318ba3281bea7c00a00599cc93484c7a
hampus <hampus.hauffman@wearelevels.com>
2022-09-21 15:09:00 +0200

    minor color updates

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,7 +1,3 @@
-
-vim.cmd [[au BufEnter * nested hi TSProperty guifg=#FFB86C]]
-vim.cmd [[au BufEnter * nested hi TSVariable guifg=#BD9F39]]
-
 -- Don't auto commenting new lines
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*",
--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -20,6 +20,9 @@ cmp.setup {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     },
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+    },
     snippet = {
         expand = function(args)
             luasnip.lsp_expand(args.body)
@@ -43,7 +46,6 @@ cmp.setup {
         format = lspkind.cmp_format({
             mode = "symbol", -- show only symbol annotations
             maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-
             -- The function below will be called before any actual modifications from lspkind
             -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
             before = function(entry, vim_item)
