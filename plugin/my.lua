7ae4c4652c5acfb83d2d3e8a76c2cc304e911785
hampus <hampus.hauffman@wearelevels.com>
2023-05-01 19:40:17 +0200

    progress

--- plugin/my.lua
+++ plugin/my.lua
@@ -32,9 +32,11 @@ end
 ---@param ts_node TSNode
 ---@param nest_nr integer
 ---@param lines string[]
-local function color_node(ts_node, nest_nr, lines, prev_max_col)
+---@param prev_col integer[]
+local function color_node(ts_node, nest_nr, lines, prev_col)
   local start_row, start_col, end_row, end_col = ts_node:range()
   local max_col = find_biggest_end_col(ts_node)
+
 
   if (start_row == end_row) then return end
   if (ts_node:type() == "arguments") then return end
@@ -42,19 +44,36 @@ local function color_node(ts_node, nest_nr, lines, prev_max_col)
   nest_nr = nest_nr + (ts_node:type() == "block" and -1 or 0)
 
   for row = start_row, end_row - (nest_nr == 0 and 1 or 0) do -- Figure out why i need this check
-    local spaces = string.rep(" ", max_col - string.len(lines[row + 1]))
-    local prev_spaces = string.rep(" ", prev_max_col - max_col)
+    local counter = nest_nr
+    local line_len = string.len(lines[row + 1])
+    local padding = max_col - line_len
+    local total_length = line_len + padding
+
+    local spaces = string.rep(" ", padding)
+    local colors = { { spaces, "Bloc" .. counter % 2 } }
+
+    for p = #prev_col, 1, -1 do
+      local prev_max_col = prev_col[p]
+      local padding = prev_max_col - total_length
+      total_length = total_length + padding
+      local s = string.rep(" ", padding)
+      table.insert(colors, { s, "Bloc" .. (counter + p) % 2 })
+    end
+
 
     -- Fill out space to  longest char (spaces)
     vim.api.nvim_buf_set_extmark(0, ns_id, row, 0, {
       id = row + 1,
-      virt_text = { { spaces, "Bloc" .. nest_nr % 2 }, { prev_spaces, "Bloc" .. (nest_nr - 1) % 2 } },
+      virt_text = colors,
       virt_text_pos = "eol",
     })
   end
 
+
   for i in ts_node:iter_children() do
-    color_node(i, nest_nr + 1, lines, max_col)
+    table.insert(prev_col, max_col)
+    color_node(i, nest_nr + 1, lines, prev_col)
+    table.remove(prev_col, #prev_col)
   end
 end
 
@@ -62,7 +81,5 @@ function M()
   local root_node = get_root_node()
   local buf_lines = vim.api.nvim_buf_line_count(0)
   local lines = vim.api.nvim_buf_get_lines(0, 0, buf_lines, true)
-  print(root_node:end_())
-
-  color_node(root_node, 0, lines, find_biggest_end_col(root_node))
+  color_node(root_node, 0, lines, {})
 end
