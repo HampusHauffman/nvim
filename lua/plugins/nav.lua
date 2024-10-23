c7bd8481d67511016b25bdbfdb4c6ff3575389c4
Hampus <hampus@hauffman.se>
2024-10-23 19:20:01 +0200

    removed uneccesary defaults

--- lua/plugins/nav.lua
+++ lua/plugins/nav.lua
@@ -38,21 +38,6 @@ M[#M + 1] = {
     default_component_configs = {
       modified = { symbol = "" },
     },
-    git_status = {
-      symbols = {
-        -- Change type
-        added = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
-        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-        deleted = "✖", -- this can only be used in the git_status source
-        renamed = "󰁕", -- this can only be used in the git_status source
-        -- Status type
-        untracked = "",
-        ignored = "",
-        unstaged = "󰄱",
-        staged = "",
-        conflict = "",
-      },
-    },
   },
 }
 
