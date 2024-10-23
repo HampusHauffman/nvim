60e599ba976ece92d762cd68025a5bf10aa28db0
Hampus <hampus@hauffman.se>
2024-10-23 19:19:34 +0200

    updated with signs

--- lua/plugins/nav.lua
+++ lua/plugins/nav.lua
@@ -34,6 +34,25 @@ M[#M + 1] = {
   },
   opts = {
     popup_border_style = "rounded",
+    close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+    default_component_configs = {
+      modified = { symbol = "" },
+    },
+    git_status = {
+      symbols = {
+        -- Change type
+        added = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+        deleted = "✖", -- this can only be used in the git_status source
+        renamed = "󰁕", -- this can only be used in the git_status source
+        -- Status type
+        untracked = "",
+        ignored = "",
+        unstaged = "󰄱",
+        staged = "",
+        conflict = "",
+      },
+    },
   },
 }
 
