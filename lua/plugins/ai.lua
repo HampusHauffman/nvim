1799e6ed2798fbb11d4628e6097a66867a108e4b
Hampus <hampus.hauffman.external@zalando.de>
2025-09-29 21:08:14 +0200

    ok

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -43,56 +43,6 @@ M[#M + 1] = {
   },
   lazy = true,
   config = true,
-  opts = {
-    strategies = {
-      chat = {
-        adapter = "openai",
-      },
-      inline = {
-        adapter = "openai",
-      },
-    },
-    adapters = {
-      opts = {
-        show_defaults = false,
-      },
-      gemini = function()
-        return require("codecompanion.adapters").extend("gemini", {
-          schema = {
-            model = {
-              default = "gemini-2.5-pro",
-            },
-          },
-        })
-      end,
-      openai = function()
-        return require("codecompanion.adapters").extend("openai_compatible", {
-          env = {
-            url = "https://zllm.data.zalan.do",
-            --api_key = get_ztoken,
-            chat_url = "/v1/chat/completions",
-          },
-          schema = {
-            model = {
-              default = "bedrock/anthropic.claude-sonnet-4-20250514-v1:0",
-            },
-            temperature = {
-              default = 0.3,
-            },
-            max_completion_tokens = {
-              default = 8000,
-            },
-          },
-        })
-      end,
-    },
-    display = {
-      chat = { show_settings = true },
-      diff = {
-        provider = "mini_diff",
-      },
-    },
-  },
   keys = require("keymaps.ai").keys,
 }
 
