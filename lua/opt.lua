0001829ec0a658d361aff13524ff2651229f4593
Hampus <hampus@Hampuss-MacBook-Pro-2.local>
2025-10-04 22:49:03 +0200

    ok

--- lua/opt.lua
+++ lua/opt.lua
@@ -27,6 +27,39 @@ opt.foldmethod = "expr"
 opt.foldlevel = 1
 opt.foldenable = false -- Use zi to enable folds
 opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+_G.FoldText = function()
+  local api = vim.api
+  local fn = vim.fn
+  -- This is the content of the first line of the fold
+  local folded_lines =
+    api.nvim_buf_get_lines(0, vim.v.foldstart - 1, vim.v.foldend, true)
+  -- Get the width of the current window.
+  --
+  local win_width = api.nvim_win_get_width(0)
+
+  local text = folded_lines[1]
+
+  local line_count_text = " " .. #folded_lines .. " 󰊠  "
+
+  text = text .. "  ┄╌"
+  line_count_text = "╌┄  " .. line_count_text
+
+  -- Calculate the number of dashes needed to fill the remaining space.
+  local available_width = win_width
+    - fn.strdisplaywidth(text)
+    - fn.strdisplaywidth(line_count_text)
+    - 6
+
+  local dashes = ""
+  dashes = string.rep("─", available_width)
+
+  -- Construct and return the final fold text.
+  return text .. dashes .. line_count_text
+end
+
+vim.o.foldtext = "v:lua.FoldText()"
+-- FoldText is the function name
 opt.virtualedit = "block"
 --opt.mopt = "wait:500,history:500" -- Show any "enter" mess for half a sec then remove
 opt.shortmess = "ltToOCFsqS"
